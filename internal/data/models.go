6fa6f2129fabfc850d552eff7504ef097e81af3c
Junior Paula <luke.junnior@icloud.com>
2024-03-16 11:38:28 -0300

    Update the handler to the logging a user out

--- internal/data/models.go
+++ internal/data/models.go
@@ -39,6 +39,7 @@ type User struct {
 	FirstName string    `json:"first_name,omitempty"`
 	LastName  string    `json:"last_name,omitempty"`
 	Password  string    `json:"password"`
+	Active    int       `json:"active"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Token     Token     `json:"token"`
@@ -49,7 +50,12 @@ func (u *User) GetAll() ([]*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-	query := `select id, email, first_name, last_name, password, created_at, updated_at from users order by first_name`
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at,
+	case
+		when (select count(id) from tokens t where user_id = users.id and t.expiry > NOW()) > 0 then 1
+		else 0
+	end as has_token
+	from users order by first_name`
 
 	rows, err := db.QueryContext(ctx, query)
 	if err != nil {
@@ -67,8 +73,10 @@ func (u *User) GetAll() ([]*User, error) {
 			&user.FirstName,
 			&user.LastName,
 			&user.Password,
+			&user.Active,
 			&user.CreatedAt,
 			&user.UpdatedAt,
+			&user.Token.ID,
 		)
 		if err != nil {
 			return nil, err
@@ -85,7 +93,7 @@ func (u *User) GetByEmail(email string) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-	query := `select id, email, first_name, last_name, password, created_at, updated_at from users where email = $1`
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from users where email = $1`
 
 	var user User
 	row := db.QueryRowContext(ctx, query, email)
@@ -96,6 +104,7 @@ func (u *User) GetByEmail(email string) (*User, error) {
 		&user.FirstName,
 		&user.LastName,
 		&user.Password,
+		&user.Active,
 		&user.CreatedAt,
 		&user.UpdatedAt,
 	)
@@ -111,7 +120,7 @@ func (u *User) GetById(id int) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-	query := `select id, email, first_name, last_name, password, created_at, updated_at from users where id = $1`
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from users where id = $1`
 
 	var user User
 	row := db.QueryRowContext(ctx, query, id)
@@ -122,6 +131,7 @@ func (u *User) GetById(id int) (*User, error) {
 		&user.FirstName,
 		&user.LastName,
 		&user.Password,
+		&user.Active,
 		&user.CreatedAt,
 		&user.UpdatedAt,
 	)
@@ -137,12 +147,13 @@ func (u *User) Update() error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-	stmt := `update users set email = $1, first_name = $2, last_name = $3, updated_at = $4 where id = $5`
+	stmt := `update users set email = $1, first_name = $2, last_name = $3, user_active = $4, updated_at = $5 where id = $6`
 
 	_, err := db.ExecContext(ctx, stmt,
 		u.Email,
 		u.FirstName,
 		u.LastName,
+		u.Active,
 		time.Now(),
 		u.ID,
 	)
@@ -194,8 +205,8 @@ func (u *User) Insert(user User) (int, error) {
 	}
 
 	var newID int
-	stmt := `insert into users (email, first_name, last_name, password, created_at, updated_at)
-		values ($1, $2, $3, $4, $5, $6) returning id
+	stmt := `insert into users (email, first_name, last_name, password, user_active created_at, updated_at)
+		values ($1, $2, $3, $4, $5, $6, $7) returning id
 	`
 
 	err = db.QueryRowContext(ctx, stmt,
@@ -203,6 +214,7 @@ func (u *User) Insert(user User) (int, error) {
 		user.FirstName,
 		user.LastName,
 		hashedPassword,
+		user.Active,
 		time.Now(),
 		time.Now(),
 	).Scan(&newID)
@@ -295,7 +307,7 @@ func (t *Token) GetUserForToken(token Token) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-	query := `select id, email, first_name, last_name, password, created_at, updated_at from users where id = $1`
+	query := `select id, email, first_name, last_name, password, user_active created_at, updated_at from users where id = $1`
 
 	var user User
 	row := db.QueryRowContext(ctx, query, token.UserID)
@@ -305,6 +317,7 @@ func (t *Token) GetUserForToken(token Token) (*User, error) {
 		&user.FirstName,
 		&user.LastName,
 		&user.Password,
+		&user.Active,
 		&user.CreatedAt,
 		&user.UpdatedAt,
 	)
@@ -365,6 +378,10 @@ func (t *Token) AuthenticateToken(r *http.Request) (*User, error) {
 	user, err := t.GetUserForToken(*tkn)
 	if err != nil {
 		return nil, errors.New("no matching user found")
+	}
+
+	if user.Active == 0 {
+		return nil, errors.New("user not active")
 	}
 
 	return user, nil
